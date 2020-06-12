3bdb2a606d4a3a0ee4ef50f910cfe812d306be19
yamassx <58455442+yamassx@users.noreply.github.com>
2020-06-12 13:48:07 +0900

    aaa

--- src/main/resources/db/migration/V1__create_menu_table.sql
+++ src/main/resources/db/migration/V1__create_menu_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE `menu`
     `menu_id`   int          NOT NULL AUTO_INCREMENT,
     `menuName` varchar(255) NOT NULL,
     `menuType` varchar(2)      NOT NULL,
+    `menuPrice` int NOT NULL,
     PRIMARY KEY (`menu_id`)
 );
 
